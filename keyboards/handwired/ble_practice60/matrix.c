0370ca17df42580fefc3173ca6bec17e491ea83d
Thomas Schucker <thomas.schucker@decatechnologies.com>
2019-05-11 01:10:58 -0700

    Updated matrix info.

--- keyboards/handwired/ble_practice60/matrix.c
+++ keyboards/handwired/ble_practice60/matrix.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Wez Furlong
+Copyright 2019 Tom Schucker
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
